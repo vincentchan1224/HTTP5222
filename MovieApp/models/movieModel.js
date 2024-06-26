b91cae2f3e9af2a0ba991320d717757ccc503bdb
Vincent Chan <vincentcahn1224@gmail.com>
2024-06-26 16:48:42 -0400

    A1 update2

---
+++ MovieApp/models/movieModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    year: { type: Number, required: true },
+    rating: { type: String, required: true }
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
